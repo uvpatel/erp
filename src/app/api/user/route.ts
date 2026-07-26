615cc4af3496d5dd85fa32f8c72d8b2f3e2a11a3
Urvil Patel <uvpatel7271@gmail.com>
2026-07-26 20:48:04 +0530

    Added Authentication

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/db/index";
+import { usersTable } from "@/db/schema";
+
+
+
+export async function GET(request: NextRequest) {
+    try {
+       const users = await db.select().from(usersTable);
+
+       return NextResponse.
+       json({ 
+        "message" : "user fetched successfully", 
+        users: users }, 
+        { status: 200 });
+    } catch (error) {
+        console.error("Error in GET /api/user:", error);
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    }
+}
